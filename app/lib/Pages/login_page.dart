63defd46ce22fac382c5595219292c319fbe0b9a
Spandan Datta <spandandatta@Spandans-MacBook-Air.local>
2024-06-13 17:16:50 -0500

    Login UI

---
+++ app/lib/Pages/login_page.dart
@@ -0,0 +1,98 @@
+
+import 'package:flutter/material.dart';
+
+class loginPage extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return _loginPageState();
+  }
+
+}
+
+class _loginPageState extends State<loginPage>{
+  GlobalKey<FormState> _loginFormKey = GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:
+         Text(
+          "Login",
+          ),
+         ),
+         body: SafeArea(
+          child: _buildUI(),
+          ),
+       );
+      }
+
+
+      Widget _buildUI(){
+        return SizedBox(
+          width: MediaQuery.sizeOf(context).width,
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              _title(),
+              _loginForm(),
+            ],
+          ),
+        );
+      }
+
+      Widget _title(){
+        return const Text(
+          "PalorahAI",
+          style: TextStyle(
+            fontSize:35,
+            fontWeight: FontWeight.w200,
+          ),
+          );
+      }
+
+      Widget _loginForm(){
+        return SizedBox(
+          width: MediaQuery.sizeOf(context).width * .90,
+          height: MediaQuery.sizeOf(context).height * .30,
+          child: Form(
+            key: _loginFormKey,
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Username",
+                  ),
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Password",
+                  ),
+                ),
+                _loginButton(),
+              ],
+            ),
+          ),
+        );
+      }
+
+      Widget _loginButton(){
+        return SizedBox(
+          width: MediaQuery.sizeOf(context).width * .60,
+          child: ElevatedButton(
+            onPressed: (){
+              Navigator.pushReplacementNamed(context, "/home");
+            },
+            child: const Text(
+              "Login",
+              ),
+          ),
+        );
+      }
+
+}
--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:app/Pages/home_page.dart';
+import 'package:app/Pages/login_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -13,96 +15,11 @@ class MyApp extends StatelessWidget {
       title: 'Custom Fonts',
       // Set Raleway as the default app font.
       theme: ThemeData(fontFamily: 'PlayfairDisplay'),
-      home: Scaffold(
-        backgroundColor: Colors.transparent, // Set background color to transparent
-        body: Stack(
-          children: [
-            // Background Image
-            Positioned.fill(
-              child: Image.asset(
-                'images/earth.jpg', // Path to your background image
-                fit: BoxFit.cover, // Cover the entire screen
-              ),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomCenter,
-                  stops: const [0.0, 0.75],
-                  colors: [
-                    Colors.green.withOpacity(0.1), // White with 50% opacity
-                    Colors.black.withOpacity(1), // Black with 50% opacity
-                  ],
-                ),
-              ),
-              child: Center(
-                child: Container(
-                  padding: const EdgeInsets.all(12),
-                  width: MediaQuery.of(context).size.width*0.9,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      const Align(
-                        alignment: Alignment.bottomCenter,
-                        child: Padding(
-                          padding: EdgeInsets.all(12.0),
-                          child: Text(
-                            'Build an organization without limits',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 25, color: Colors.white),
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
-                      ),
-                      const Flexible(
-                        // padding: EdgeInsets.only(top: 20, bottom: 20, left: 30, right: 30),
-                        child: Text(
-                          "Build and scale with confidence. From a powerful" +
-                              " financial advisor to advanced organization solutions—we’ve got you covered",
-                          style: TextStyle(
-                            color: Color.fromARGB(255, 255, 255, 255),
-                            fontSize: 12,
-                          ),
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                      Align(
-                        alignment: Alignment.bottomCenter,
-                        child: Padding(
-                          padding: const EdgeInsets.only(bottom: 30, top: 20),
-                          child: ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              shadowColor: Colors.greenAccent,
-                              foregroundColor: Colors.black,
-                              backgroundColor: const Color(0xFF7F9289),
-                              elevation: 3,
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(32.0)),
-                              minimumSize: const Size(250, 60),
-                            ),
-                            onPressed: () {},
-                            child: const Padding(
-                              padding: EdgeInsets.all(6.0),
-                              child: Text(
-                                'Get Started',
-                                style: TextStyle(
-                                    color: Color(0xFF25301E),
-                                    fontSize: 25,
-                                    fontWeight: FontWeight.w500),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
+      initialRoute: "/login",
+      routes: {
+        "/login": (context) => loginPage(),
+        "/home": (context) =>homePage(),
+      },
     );
   }
 }
