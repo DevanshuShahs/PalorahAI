872491fb1ac4ced696740fd1087c58a47d0fb1d2
Nytroid <contactdevanshu2007@gmail.com>
2024-06-13 14:21:03 -0500

    y

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
             // Background Image
             Positioned.fill(
               child: Image.asset(
-                'images/plant.jpg', // Path to your background image
+                'images/earth.jpg', // Path to your background image
                 fit: BoxFit.cover, // Cover the entire screen
               ),
             ),
@@ -29,67 +29,74 @@ class MyApp extends StatelessWidget {
                 gradient: LinearGradient(
                   begin: Alignment.topLeft,
                   end: Alignment.bottomCenter,
-                  stops: [0.0, 0.85],
+                  stops: const [0.0, 0.75],
                   colors: [
-                    Colors.green.withOpacity(0), // White with 50% opacity
+                    Colors.green.withOpacity(0.1), // White with 50% opacity
                     Colors.black.withOpacity(1), // Black with 50% opacity
                   ],
                 ),
               ),
               child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    const Align(
-                      alignment: Alignment.bottomCenter,
-                      child: Padding(
-                        padding: EdgeInsets.all(12.0),
+                child: Container(
+                  padding: const EdgeInsets.all(12),
+                  width: MediaQuery.of(context).size.width*0.9,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      const Align(
+                        alignment: Alignment.bottomCenter,
+                        child: Padding(
+                          padding: EdgeInsets.all(12.0),
+                          child: Text(
+                            'Build an organization without limits',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 25, color: Colors.white),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ),
+                      const Flexible(
+                        // padding: EdgeInsets.only(top: 20, bottom: 20, left: 30, right: 30),
                         child: Text(
-                          'Build an organization without limits',
+                          "Build and scale with confidence. From a powerful" +
+                              " financial advisor to advanced organization solutions—we’ve got you covered",
                           style: TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 30, color: Colors.white),
+                            color: Color.fromARGB(255, 255, 255, 255),
+                            fontSize: 12,
+                          ),
                           textAlign: TextAlign.center,
                         ),
                       ),
-                    ),
-                    const Padding(
-                      padding: EdgeInsets.all(20),
-                      child: Text(
-                        "Build and scale with confidence. From a powerful" +
-                            " financial advisor to advanced organization solutions—we’ve got you covered",
-                        style: TextStyle(
-                          color: Color.fromARGB(255, 255, 255, 255),
-                          fontSize: 10,
-                        ),
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                    Align(
-                      alignment: Alignment.bottomCenter,
-                      child: Padding(
-                        padding: const EdgeInsets.all(20),
-                        child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            shadowColor: Colors.greenAccent,
-                            foregroundColor: Colors.black,
-                            backgroundColor: const Color(0xFF7F9289),
-                            elevation: 3,
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(32.0)),
-                            minimumSize: Size(250, 60),
-                          ),
-                          onPressed: () {},
-                          child: Text(
-                            'Get Started',
-                            style: TextStyle(
-                                color: const Color(0xFF25301E),
-                                fontSize: 25,
-                                fontWeight: FontWeight.w500),
+                      Align(
+                        alignment: Alignment.bottomCenter,
+                        child: Padding(
+                          padding: const EdgeInsets.only(bottom: 30, top: 20),
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              shadowColor: Colors.greenAccent,
+                              foregroundColor: Colors.black,
+                              backgroundColor: const Color(0xFF7F9289),
+                              elevation: 3,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(32.0)),
+                              minimumSize: const Size(250, 60),
+                            ),
+                            onPressed: () {},
+                            child: const Padding(
+                              padding: EdgeInsets.all(6.0),
+                              child: Text(
+                                'Get Started',
+                                style: TextStyle(
+                                    color: Color(0xFF25301E),
+                                    fontSize: 25,
+                                    fontWeight: FontWeight.w500),
+                              ),
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
